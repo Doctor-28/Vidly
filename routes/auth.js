8074b2b3dae6e85452c831439a4e84e4fae34695
Doctor-28 <leoberlatto@gmail.com>
2018-12-16 15:30:05 -0200

    refactoring application with services

--- routes/auth.js
+++ routes/auth.js
@@ -1,36 +1,20 @@
-const router = require('express').Router()
-const bcrypt = require('bcrypt')
-const _ = require('lodash');
-const Joi = require('joi');
-const PasswordComplexity = require('joi-password-complexity');
-const { User, complexityOptions } = require('../models/user');
-
+const router = require('express').Router();
+const {
+    login
+} = require('../services/auth');
 
 router.post('/', async (req, res) => {
-    const { error } = validateUser(req.body);
-    if (error)
-        return res.status(400).send(error.details[0].message);
-
-    let user = await User.findOne({ email: req.body.email });
-    if (!user)
-        return res.status(400).send('User with this email not found')
-
-    const validPassword = await bcrypt.compare(req.body.password, user.password)
-    if (!validPassword)
-        return res.status(400).send('Invalid password')
-
-
-    const token = user.generateAuthToken();
-    res.send({ token: token })
+    try {
+        const user = await login(req.body);
+        const token = user.generateAuthToken();
+        res.send({
+            token: token
+        })
+    } catch (error) {
+        res.status(error.httpStatus)
+        res.send(error.message)
+    }
 });
-
-function validateUser(req) {
-    const schema = {
-        email: Joi.string().min(6).max(50).email().required(),
-        password: new PasswordComplexity(complexityOptions)
-    };
-    return Joi.validate(req, schema);
-};
 
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,46 +1,43 @@
-const bcrypt = require('bcrypt');
 const _ = require('lodash');
 const auth = require('../middleware/auth');
 const router = require('express').Router();
-const { User, validateUser } = require('../models/user');
+const {
+    getAll,
+    getOne,
+    getMe,
+    createUser
+} = require('../services/user');
 
 router.get('/', async (req, res) => {
-    const users = await User
-        .find()
-        .sort('name')
+    const users = await getAll();
     res.send(users);
 });
 
- router.get('/:id', async (req, res) => {
-    const user = await User.findById(req.params.id)
-    if (!user)
-        return res.status(404).send('Not found.');
-
-    res.send(user);
+router.get('/:id', async (req, res) => {
+    try {
+        const user = await getOne(req.params.id);
+        res.send(user);
+    } catch (error) {        
+        res.status(error.httpStatus)
+        res.send(error.message)
+    }
 });
 
-router.get('/me',  auth, async (req,res)=>{
-    const user = await User.findById(req.user._id).select('-password');
+router.get('/me', auth, async (req, res) => {
+    const user = await getMe(req.user._id);
     res.send(user);
 });
 
 router.post('/', async (req, res) => {
-    const { error } = validateUser(req.body);
-    if (error)
-        return res.status(400).send(error.details[0].message);
-
-    let user = await User.findOne({ email: req.body.email });
-    if (user)
-        return res.status(400).send('User already registered.')
-
-    user = new User(_.pick(req.body, ['name','email','password']));
-    const salt = await bcrypt.genSalt(10);
-    user.password = await bcrypt.hash(user.password,salt);
-    await user.save();
-
-    const token = user.generateAuthToken();
-    res.header('x-auth-token', token);
-    res.send(_.pick(user,['name','email']));
+    try {
+        const user = await createUser(req.body);
+        const token = user.generateAuthToken();
+        res.header('authorization', token);
+        res.send(_.pick(user, ['name', 'email']));
+    } catch (error) {
+        res.status(error.httpStatus)
+        res.send(error.message)
+    }
 });
 
 module.exports = router;
