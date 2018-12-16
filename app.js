f5225eff3319ad0e53b6823af3f4feafbd126273
Doctor-28 <leoberlatto@gmail.com>
2018-12-16 15:25:53 -0200

    adding morgan to the project

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const winston = require('winston');
+const morgan = require('morgan');
 const express = require('express');
 const app = express();
+
+app.use(morgan('dev'));
 
 require('./startup/routes')(app);
 require('./startup/db')();
 require('./startup/logging')();
 require('./startup/config')();
 require('./startup/validation')();
-
 
 const port = process.env.PORT || 9000;
 app.listen(port, () => {
